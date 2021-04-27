5f6daefa094e25e105d2835afc24a9f4c273ba88
Elenagerman619 <82209704+Elenagerman619@users.noreply.github.com>
2021-04-27 19:50:09 +0300

    Обновили функционал бота
    1.Переходит на целевой сайт и переходит по ссылкам с вероятностью 30% переходит на яндекс
    2.Вбивает фразу по буквам
    3.Имеет ограничение на глубину поиска не далее этой страницы

--- Yandex.bot.js
+++ Yandex.bot.js
@@ -5,7 +5,7 @@
 // @description  try to take over the world!
 // @author       German
 // @match        https://yandex.ru/*
-// @icon
+// @match        https://xn----7sbab5aqcbiddtdj1e1g.xn--p1ai/*
 // @grant        none
 // ==/UserScript==
 
@@ -13,21 +13,57 @@ let keywords = ["гобой", "саксофон", "как звучит флей�
 
 
 let button = document.getElementsByClassName ('button')[0];
-let links = document.links
+let links = document.links;
 let keyword = keywords[getRandom(0,keywords.length)];
+let yandexInput = document.getElementById("text");
+let i = 0;
 
 if(button !==undefined) {
-    document.getElementById("text").value = keyword;
-   document.getElementsByClassName('button')[0].click();
-}else{
+    let timerId = setInterval(()=> {
+        yandexInput.value += keyword[i];
+        i++;
+        if (i == keyword.length){
+            clearInterval(timerId);
+            button.click();
+        }
+    }, 1000);
+
+
+
+
+}else if(location.hostname == "xn----7sbab5aqcbiddtdj1e1g.xn--p1ai") {
+    console.log("мы на месте");
+    setTimeout(()=>{
+        let index = getRandom(0,links.length);
+        if(getRandom(0,101)>=70) {
+            location.href = "https://yandex.ru/";
+        }
+        if (links[index].href.indexOf('xn----7sbab5aqcbiddtdj1e1g.xn--p1ai')!=-1)
+            links[index].click();
+    },getRandom(2000,3500));
+}
+else {
+    let nextYandexPage = true;
     for (let i=0;i<links.length;i++){
-      if (links[i].href.indexOf('xn----7sbab5aqcbiddtdj1e1g.xn--p1ai')!=-1) {
-           let link = links[i];
-        console.log("Нашел фразу" + links[i]);
-          link.removeAttribute("target");
-          link.click();
-          break;
-      }
+        if (links[i].href.indexOf('xn----7sbab5aqcbiddtdj1e1g.xn--p1ai')!=-1) {
+            let link = links[i];
+            let nextYandexPage = false;
+            console.log("Нашел фразу" + links);
+            link.removeAttribute("target");
+            setTimeout(()=>{
+                link.click();}
+                       ,getRandom(1000,4500));
+            break;
+        }
+    }
+    if(document.querySelector('.pager__item_current_yes').textContent == "5") {
+        nextYandexPage = false;
+        location.href = "https://yandex.ru/";
+    }
+    if(document.querySelector('.pager__item_current_yes').textContent !== "5") {
+        setTimeout(()=>{
+            document.querySelector('.pager__item_kind_next').click();}
+                   ,getRandom(3000,5000));
     }
 }
 
